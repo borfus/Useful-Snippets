34f24e1d34095bba61e8af6118ed4005af8adb5b
borfus <mrpeteyb@gmail.com>
2020-04-18 18:07:21 -0500

    added return 1 to pid failure in get_pid_and_open_process.c and added get_pid.c

--- get_pid.c
+++ get_pid.c
@@ -13,6 +13,7 @@ DWORD search_process_list_for_pid(HANDLE process_list, wchar_t* process_name) {
             }
         }
     }
+
     return 0;
 }
 
@@ -21,29 +22,14 @@ DWORD get_pid(wchar_t* process_name) {
     if (process_list != NULL) {
         return search_process_list_for_pid(process_list, process_name);
     }
+
     return 0;
 }
 
-HANDLE open_process(DWORD pid) {
-    HANDLE process = OpenProcess(PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE, FALSE, pid);
-    return process;
-}
-
-int main() {
+int main()
+{
     wchar_t process_name[32] = L"notepad.exe";
     DWORD pid = get_pid(process_name);
-    if (!pid) {
-        printf("Unable to get PID of process \"%ls\"\n", process_name);
-        return 1;
-    }
-
-    HANDLE process = open_process(pid);
-    if (process == INVALID_HANDLE_VALUE) {
-        printf("Failed to open PID %d, error code %d\n", pid, GetLastError());
-        return 1;
-    }
-
-    printf("Process %ls opened with PID: %d\n", process_name, pid);
-
+    printf("%ls PID: %d\n", process_name, pid);
     return 0;
 }
