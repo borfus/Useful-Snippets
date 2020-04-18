fa444a1e3dbea6fb93bde4865cc591205afd91c5
borfus <mrpeteyb@gmail.com>
2020-04-18 18:46:56 -0500

    Added protect_memory 'generic-like' macro

--- generic_memory_manipulation.h
+++ generic_memory_manipulation.h
@@ -12,12 +12,25 @@ void write_memory_##T(HANDLE process, LPVOID address, T val) { \
 	WriteProcessMemory(process, address, &val, sizeof(T), NULL); \
 }
 
+#define define_protect_memory(T) \
+DWORD protect_memory_##T(HANDLE process, LPVOID address, DWORD prot) { \
+    DWORD old_prot; \
+    VirtualProtectEx(process, address, sizeof(T), prot, &old_prot); \
+    return old_prot; \
+}
+
 #define read_memory(T) read_memory_##T
 #define write_memory(T) write_memory_##T
+#define protect_memory(T) protect_memory_##T
 
 define_read_memory(DWORD)
 define_write_memory(DWORD)
+define_protect_memory(DWORD)
 
 // example usage after defines
-// DWORD val = read_memory(DWORD)(proc, adr)
-// write_memory(DWORD)(proc, adr, val)
+// DWORD val = read_memory(DWORD)(proc, adr);
+// write_memory(DWORD)(proc, adr, val);
+
+// DWORD old_prot = protect_memory(DWORD)(proc, adr, PAGE_READWRITE);    // Might want PAGE_EXECUTE_READWRITE to prevent some game crashing
+// write_memory(DWORD)(proc, adr, new_val);
+// protect_memory(DWORD)(proc, adr, old_prot);
