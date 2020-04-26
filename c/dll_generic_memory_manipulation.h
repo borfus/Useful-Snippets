457c2f216a3743fb736a5ff8e291e3fe841deb74
borfus <mrpeteyb@gmail.com>
2020-04-26 18:15:21 -0500

    Added jump_through_pointers function

--- c/dll_generic_memory_manipulation.h
+++ c/dll_generic_memory_manipulation.h
@@ -23,9 +23,40 @@ DWORD dll_protect_memory_##T(LPVOID address, DWORD prot) { \
 }
 
 DWORD dll_protect_memory_bytes(LPVOID address, DWORD prot, int size) {
-	DWORD old_prot; 
+	DWORD old_prot;
 	VirtualProtect(address, size, prot, &old_prot);
-	return old_prot; 
+	return old_prot;
+}
+
+void dll_write_memory_bytes(LPVOID address, BYTE* val, int size) {
+	DWORD old_prot = dll_protect_memory_bytes((LPVOID)address, PAGE_EXECUTE_READWRITE, size);
+
+	for (int i = 0; i < size; i++) {
+		*((BYTE*)(address)+i) = *(val + i);
+	}
+
+	dll_protect_memory_bytes((LPVOID)address, old_prot, size);
+}
+
+void dll_nop_memory(DWORD address, int size) {
+	DWORD old_prot = dll_protect_memory_bytes((LPVOID)address, PAGE_EXECUTE_READWRITE, size);
+	BYTE nop_byte = 0x90;
+
+	for (int i = 0; i < size; i++) {	
+		dll_write_memory_bytes((LPVOID)(address + i), &nop_byte, 1);
+	}
+
+	dll_protect_memory_bytes((LPVOID)address, old_prot, size);
+}
+
+DWORD* dll_jump_through_pointers(DWORD* pointer_list, int list_size) {
+	DWORD current_value = *(DWORD*)*pointer_list;
+
+	for (int i = 1; i < list_size; i++) {
+		current_value = *(DWORD*)(current_value + *(DWORD*)(pointer_list + i));
+	}
+
+	return (DWORD*)current_value;
 }
 
 #define dll_read_memory(T) dll_read_memory_##T
@@ -38,16 +69,6 @@ define_dll_write_memory(DWORD)
 define_dll_write_memory(BYTE)
 define_dll_read_memory_pointer(DWORD)
 define_dll_protect_memory(DWORD)
-
-void dll_nop_memory(DWORD address, int size) { 
-	DWORD old_prot = dll_protect_memory_bytes((LPVOID)address, PAGE_EXECUTE_READWRITE, size); 
-
-	for (int i = 0; i < size; i++) { 
-		dll_write_memory(BYTE)((LPVOID)(address + i), 0x90); 
-	}
-
-	dll_protect_memory_bytes((LPVOID)address, old_prot, size); 
-}
 
 // EXAMPLES
 // DWORD val = dll_read_memory(DWORD)(adr);
